df01f61c613fd703465c11abb73bf17392d2f7de
tammerb <tammer.barkouki@gmail.com>
2023-12-11 10:51:32 -0800

    create new env for joint controller

--- agents/ur5e_position_sb_sac.py
+++ agents/ur5e_position_sb_sac.py
@@ -11,12 +11,12 @@ env = gymnasium.make('manipulator_mujoco/UR5eSBEnvPos-v0', render_mode='human')
 ### https://stable-baselines.readthedocs.io/en/master/modules/sac.html
 model = SAC("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=10000, log_interval=4)
-model.save("sac_ur5e_pos_3")
+model.save("sac_ur5e_pos_5")
 del model # remove to demonstrate saving and loading
 
 print("Done training...")
 time.sleep(5)
-model = SAC.load("sac_ur5e_pos_3")
+model = SAC.load("sac_ur5e_pos_5")
 
 #print("***********Evaluating the model... ***********")
 #mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
