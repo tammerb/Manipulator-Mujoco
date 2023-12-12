4b76eead6622c50d8b1cd9a6261e466eae1149c8
tammerb <tammer.barkouki@gmail.com>
2023-12-12 14:37:25 -0800

    register velocity control and peg in hole envs

--- manipulator_mujoco/__init__.py
+++ manipulator_mujoco/__init__.py
@@ -32,3 +32,19 @@ register(
     # max_episode_steps=300,
     # TODO: Uncomment the above line if you want to set a maximum episode step limit
 )
+
+register(
+    id="manipulator_mujoco/UR5eSBEnvVel-v0",
+    entry_point="manipulator_mujoco.envs:UR5eSBEnvVel",
+    # Optionally, you can set a maximum number of steps per episode
+    # max_episode_steps=300,
+    # TODO: Uncomment the above line if you want to set a maximum episode step limit
+)
+
+register(
+    id="manipulator_mujoco/UR5eSBEnvVelPIH-v0",
+    entry_point="manipulator_mujoco.envs:UR5eSBEnvVelPIH",
+    # Optionally, you can set a maximum number of steps per episode
+    # max_episode_steps=300,
+    # TODO: Uncomment the above line if you want to set a maximum episode step limit
+)
